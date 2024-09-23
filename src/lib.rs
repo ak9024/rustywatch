dcfb419d4a436bcfc312d6aa26dffa4b8babce6b
Adiatma Kamarudin <adiatma.mail@gmail.com>
2024-09-23 19:07:13 +0800

    chore: add more testing

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod logger;
 pub mod watch;
 
 use log::error;
-use std::error::Error;
+use std::{error::Error, process};
 
 pub async fn run(
     dir: String,
@@ -12,7 +12,13 @@ pub async fn run(
     ignore: Option<Vec<String>>,
     bin_path: Option<String>,
     bin_arg: Option<Vec<String>>,
+    test: bool,
 ) -> Result<(), Box<dyn Error + Send>> {
+    // for testing to prevent blocking
+    if test {
+        process::exit(0)
+    }
+
     if let Err(err) = watch::watch(dir, cmd, ignore, bin_path, bin_arg).await {
         error!("Error watching directory: {:?}", err)
     }
@@ -24,6 +30,25 @@ pub async fn run(
 mod tests {
     #[tokio::test]
     async fn test_run() {
-        // TODO: Implement test cases
+        use tempfile::tempdir;
+
+        // Create a temporary directory for testing
+        let temp_dir = tempdir().unwrap();
+        let dir_path = temp_dir.path().to_str().unwrap().to_string();
+
+        // Test parameters
+        let cmd = "echo 'File changed'".to_string();
+        let ignore = Some(vec!["*.tmp".to_string()]);
+        let bin_path = Some("/usr/bin/echo".to_string());
+        let bin_arg = Some(vec!["Hello".to_string()]);
+
+        // Run the function
+        let result = super::run(dir_path, cmd, ignore, bin_path, bin_arg, true).await;
+
+        // Assert that the function returns Ok(())
+        assert!(result.is_ok());
+
+        // Clean up temporary directory
+        temp_dir.close().unwrap();
     }
 }
