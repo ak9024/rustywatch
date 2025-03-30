b7a79f499f5986a4fe67df879cc389365499fe8c
Adiatma Kamarudin <adiatma.mail@gmail.com>
2025-03-30 22:17:45 +0700

    chore: setup nix for dev shell

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # Rust toolchain
+    rustc
+    cargo
+    rustfmt
+    clippy
+    rust-analyzer
+    
+    # Build essentials
+    pkg-config
+    openssl
+    openssl.dev
+    
+    # Development tools
+    lldb
+    gdb
+    
+    # Version control
+    git
+    
+    # Documentation
+    rustup
+  ];
+
+  shellHook = ''
+    echo "Rust development environment loaded!"
+    export RUST_BACKTRACE=1
+  '';
+
+  # Set environment variables if needed
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
